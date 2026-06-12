d26bfbfd65c13f98fa0896cc2f4957fdd6117670
cb715 <112981370+CB715@users.noreply.github.com>
2026-06-12 16:48:47 +0100

    formatting

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,19 +1,13 @@
 const repeatString = function(string, num) {
-    // let temp = '';
     let result = '';
+    
     if (num < 0) {
         return "ERROR"
     }
 
-    // if (num == 0 || string =='') {
-    //     return '';
-    // }
-
     for (let i = 0; i < num; i++) {
-        // temp = temp + string;
         result += string;
     }
-    // return temp;
     return result;
 };
 
