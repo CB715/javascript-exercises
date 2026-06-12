ea86bfac0cada743c12601690adee135786a6dff
cb715 <112981370+CB715@users.noreply.github.com>
2026-06-12 23:36:31 +0100

    temperature converter updated

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+farenheitToCelsius = Math.round(((temp - 32) * 5/9) * 10) / 10;
+return farenheitToCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  celsiusToFarenheit = Math.round(((temp * 9/5) + 32) * 10) / 10;
+  return celsiusToFarenheit;
 };
 
 // Do not edit below this line
