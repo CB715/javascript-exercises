0701a83483710916e023fa9b3d63a6a9341e51c9
cb715 <112981370+CB715@users.noreply.github.com>
2026-06-12 19:58:30 +0100

    remove item from array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+    let modifiedArray = [];
+    argsArray = Array.from(theArgs);
+    
+    for (let i = 0; i < array.length; i++) {
+        if (!argsArray.includes(array[i]))
+            modifiedArray.push(array[i]);
+    }
+    
+    return modifiedArray;
 };
 
 // Do not edit below this line
