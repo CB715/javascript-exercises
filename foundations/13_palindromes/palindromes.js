495aa6221413d518c5fe70becd59aeaa22ef5b44
cb715 <112981370+CB715@users.noreply.github.com>
2026-06-16 23:53:48 +0100

    palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (input) {
 
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+    const newString = input.toLowerCase().split("").filter((char) => chars.includes(char)).join("");
+    let reversed = newString.split("").reverse().join("");
+
+    return newString == reversed;
 };
 
 // Do not edit below this line
