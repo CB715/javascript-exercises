04b65ea06a931d116c010ed09fae3c8df242dfa6
cb715 <112981370+CB715@users.noreply.github.com>
2026-06-16 19:17:25 +0100

    calculator updated

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sumOfArray = 0;
+	array.forEach(element => { sumOfArray += element });
+  return sumOfArray;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let multi = 1;
+  arr.forEach(num => {multi *= num});
+  return multi;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let result = 1;
+  for (let i = 0; i < b; i++) {
+    result *= a;
+  };
+  return result;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(n) {
+	if (n <= 1) {
+    return 1;
+  } else {
+  return n * factorial(n - 1);
+  }
+  };
+
 
 // Do not edit below this line
 module.exports = {
