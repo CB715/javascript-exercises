eff17ac13dc56e95192b6115db8c5c1df05c88d7
cb715 <112981370+CB715@users.noreply.github.com>
2026-06-17 15:02:33 +0100

    fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
 
+let updatedInput;
+let firstValue = 1;
+let secondValue = 0;
+let current = 0;
+// check fibonacci sequence and start at [0] and [1], update variables with for loop < updatedInput
+
+    if (typeof(input) != "number") {
+        updatedInput = parseInt(input)
+    } else {
+        updatedInput = input;
+    };
+
+    if (updatedInput < 0) { 
+        return "OOPS";
+    }
+
+    for (let i = 0; i < updatedInput; i++) {
+    current = firstValue + secondValue;
+    firstValue = secondValue;
+    secondValue = current;
+    }
+    
+    return current;
 };
 
 // Do not edit below this line
